7cfce934c9b5a7453616c17aef142e398132d0ce
Lort974 <laurent.bigey@outlook.fr>
2024-03-25 10:14:38 +0100

    ajout de babel

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as DataTable } from "./components/DataTable";
+export { default as DataTableBody } from "./components/DataTableBody";
+export { default as DataTableFilter } from "./components/DataTableFilter";
+export { default as DataTableHead } from "./components/DataTableHead";
+export { default as DataTableInfo } from "./components/DataTableInfo";
+export { default as DataTableLength } from "./components/DataTableLength";
+export { default as DataTablePaginate } from "./components/DataTablePaginate";
+export { default as DataTableWrapper } from "./components/DataTableWrapper";
